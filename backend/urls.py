6b270eeaa983e5c44bdfb48ffedd225bf3202ab2
banafshaahmadyar <ahmadyarbanafsha@gmail.com>
2023-08-23 10:01:20 +0000

    cleared jwt token

--- backend/urls.py
+++ backend/urls.py
@@ -15,18 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from .views import root_route, logout_route
+from .views import root_route
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('students.urls')),
-    path('dj-rest-auth/logout/', logout_route),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path(
-        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
-    ),
     path('backend-auth/', include('rest_framework.urls')),
     path('', root_route),
 
