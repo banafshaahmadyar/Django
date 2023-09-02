91961563d9f09a5ded7b2f7166f8c2ec6f2848d8
banafshaahmadyar <ahmadyarbanafsha@gmail.com>
2023-09-02 12:39:21 +0000

    added model urls serallizer and views for registration pade

--- students/urls.py
+++ students/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .views import StudentView
+from . import views
+
 
 urlpatterns = [
     path("students/", StudentView.as_view()),
-    path("students/<int:pk>/", StudentView.as_view())
+    path("students/<int:pk>/", StudentView.as_view()),
+    path('register', views.UserRegister.as_view(), name='register'),
+	path('login', views.UserLogin.as_view(), name='login'),
+	path('logout', views.UserLogout.as_view(), name='logout'),
+	path('user', views.UserView.as_view(), name='user'),
 ]
